ff73a3416d2a707f160412de991dcd2aa21a7094
Jig3008 <jigar.updates@gmail.com>
2026-01-23 22:45:04 +0100

    Day2and3 Code Commit

---
+++ TypeCheck.py
@@ -0,0 +1,8 @@
+#Type Check for all primitive data types
+
+print(type('abc'))
+print(type(123_456))
+print(type(123_456.123))
+print(type(True))
+# print('Number of letters in your name:'+str(len(input('Enter your name:'))))
+print(3-3+3/3*3)
